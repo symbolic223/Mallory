b5eda2fed94520daa9199b8bfef213dfc3aeb15d
symbolic223 <danildemin00@gmail.com>
2024-07-24 00:29:07 +0700

    белоголов пятнистый

--- cogs/orders.py
+++ cogs/orders.py
@@ -26,24 +26,26 @@ class OrdersCog(commands.Cog):
         order = cursor.fetchone()
         cursor.execute("SELECT orders_count FROM uorders WHERE user_id = ?", (user.id,))
         uorder = cursor.fetchone()
-        if order == None:
-            await ctx.reply("Не нашла челикса в таблице. Пускай заказ сделает чтоле.")
-            return
 
+
+
+
+        e = disnake.Embed(
+            title="Кол-во ваших заказов" if user == ctx.author else f"Кол-во заказов пользователя {user.name}",
+            color=0xFF00E4
+        )
+        if order == None:
+            e.add_field(name="Я трахнула тебя:" if user == ctx.author else f"Я трахнула его:", value=f"Еще не трахала")
         else:
             orders = order[0]
-            e = disnake.Embed(
-                title="Кол-во ваших заказов" if user == ctx.author else f"Кол-во заказов пользователя {user.name}",
-                color=0xFF00E4
-            )
             e.add_field(name="Я трахнула тебя:" if user == ctx.author else f"Я трахнула его:", value=f"{orders} раз")
-            if uorder == None:
-                e.add_field(name="Заказы:", value="Пользователь не делал заказов.")
-            else:
-                uorders = uorder[0]
-                e.add_field(name="Заказы:", value=uorders)
-            e.set_thumbnail(url = user.avatar)
-            await ctx.reply(embed=e)
+        if uorder == None:
+            e.add_field(name="Заказы:", value="Пользователь не делал заказов.")
+        else:
+            uorders = uorder[0]
+            e.add_field(name="Заказы:", value=uorders)
+        e.set_thumbnail(url = user.avatar)
+        await ctx.reply(embed=e)
 
 def setup(bot: commands.Bot):
     bot.add_cog(OrdersCog(bot))
